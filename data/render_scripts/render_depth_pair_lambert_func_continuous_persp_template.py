142cab5f912ef1a2b6d0f18d253c78c4185e6e90
徐辰曦 <xcx@xuchenxideMBP.wv.cc.cmu.edu>
2019-09-21 20:50:12 -0400

    .

--- data/render_scripts/render_depth_pair_lambert_func_continuous_persp_template.py
+++ data/render_scripts/render_depth_pair_lambert_func_continuous_persp_template.py
@@ -152,222 +152,238 @@ def obj_centered_camera_pos(dist, azimuth_deg, elevation_deg):
     z = (dist * math.sin(phi))
     return (x, y, z)
 
-CATEGORY = sys.argv[-5]
-MODEL = sys.argv[-4]
-NAME = sys.argv[-3]
-RESOLUTION = int(sys.argv[-2])
-#VIEWS = int(sys.argv[-2])
-BUFFER = int(sys.argv[-1])
+def run(CATEGORY, MODEL, NAME):
+    CATEGORY = sys.argv[-5]
+    MODEL = sys.argv[-4]
+    NAME = sys.argv[-3]
+    RESOLUTION = 128
+    # VIEWS = int(sys.argv[-2])
+    BUFFER = int(sys.argv[-1])
 
-# redirect output to log file
-logfile = BASE_OUT_DIR+"tmp/blender_render_model%d.log"%BUFFER
+    # redirect output to log file
+    logfile = BASE_OUT_DIR + "tmp/blender_render_model%d.log" % BUFFER
 
-## Blender settings
-scene = bpy.context.scene
-camera = bpy.data.objects["Camera"]
-camera.data.type = "PERSP"
-print('aad', camera.data.lens, 'sensor_HW', camera.data.sensor_height, camera.data.sensor_width)
-camera.data.lens = 92
-#camera.data.sensor_width = 49.303
-camera.data.sensor_width = 49.303
-camera.data.sensor_height = camera.data.sensor_width
+    ## Blender settings
+    scene = bpy.context.scene
+    camera = bpy.data.objects["Camera"]
+    camera.data.type = "PERSP"
+    print('aad', camera.data.lens, 'sensor_HW', camera.data.sensor_height, camera.data.sensor_width)
+    camera.data.lens = 92
+    # camera.data.sensor_width = 49.303
+    camera.data.sensor_width = 49.303
+    camera.data.sensor_height = camera.data.sensor_width
 
-# compositor nodes
-scene.render.use_antialiasing = True
-scene.render.alpha_mode = "TRANSPARENT"
-scene.render.image_settings.color_depth = "16"
-scene.render.image_settings.color_mode = "RGBA"
-# scene.render.image_settings.file_format = "JPEG"
-# scene.world.horizon_color = [255, 255, 255]
-scene.render.image_settings.use_zbuffer = True
-scene.render.use_compositing = True
-scene.use_nodes = True
-tree = scene.node_tree
-for n in tree.nodes:
-    tree.nodes.remove(n)
-rl = tree.nodes.new("CompositorNodeRLayers")
-fo = tree.nodes.new("CompositorNodeOutputFile")
-fo.base_path = BASE_OUT_DIR + "tmp/buffer/{0}/".format(BUFFER)
-if not os.path.isdir(fo.base_path):
-    os.makedirs(fo.base_path)
-fo.format.file_format = "OPEN_EXR"
-fo.file_slots.new("RGB")
-fo.file_slots.new("Z")
-fo.file_slots.new("NORM")
-tree.links.new(rl.outputs["Image"],fo.inputs["RGB"])
-tree.links.new(rl.outputs["Z"],fo.inputs["Z"])
-tree.links.new(rl.outputs["Normal"],fo.inputs["NORM"])
-#print(rl.outputs.keys())
-#sys.exit()
+    # compositor nodes
+    scene.render.use_antialiasing = True
+    scene.render.alpha_mode = "TRANSPARENT"
+    scene.render.image_settings.color_depth = "16"
+    scene.render.image_settings.color_mode = "RGBA"
+    # scene.render.image_settings.file_format = "JPEG"
+    # scene.world.horizon_color = [255, 255, 255]
+    scene.render.image_settings.use_zbuffer = True
+    scene.render.use_compositing = True
+    scene.use_nodes = True
+    tree = scene.node_tree
+    for n in tree.nodes:
+        tree.nodes.remove(n)
+    rl = tree.nodes.new("CompositorNodeRLayers")
+    fo = tree.nodes.new("CompositorNodeOutputFile")
+    fo.base_path = BASE_OUT_DIR + "tmp/buffer/{0}/".format(BUFFER)
+    if not os.path.isdir(fo.base_path):
+        os.makedirs(fo.base_path)
+    fo.format.file_format = "OPEN_EXR"
+    fo.file_slots.new("RGB")
+    fo.file_slots.new("Z")
+    fo.file_slots.new("NORM")
+    tree.links.new(rl.outputs["Image"], fo.inputs["RGB"])
+    tree.links.new(rl.outputs["Z"], fo.inputs["Z"])
+    tree.links.new(rl.outputs["Normal"], fo.inputs["NORM"])
+    # print(rl.outputs.keys())
+    # sys.exit()
 
-# set environment lighting
-scene.world.light_settings.use_ambient_occlusion = True # https://docs.blender.org/manual/en/dev/render/blender_render/world/ambient_occlusion.html
-# scene.world.light_settings.use_environment_light = True
-# scene.world.light_settings.environment_energy = 10
-# scene.world.light_settings.environment_color = "PLAIN"
+    # set environment lighting
+    scene.world.light_settings.use_ambient_occlusion = True  # https://docs.blender.org/manual/en/dev/render/blender_render/world/ambient_occlusion.html
+    # scene.world.light_settings.use_environment_light = True
+    # scene.world.light_settings.environment_energy = 10
+    # scene.world.light_settings.environment_color = "PLAIN"
 
-# scene.render.resolution_x = RESOLUTION
-# scene.render.resolution_y = RESOLUTION
-# scene.render.resolution_percentage = 100
+    # scene.render.resolution_x = RESOLUTION
+    # scene.render.resolution_y = RESOLUTION
+    # scene.render.resolution_percentage = 100
 
-## Start rendering
-timeStart = time.time()
-q_extr_list = []
-t_extr_list = []
-angles_list = []
-t_list = []
-R_extr_list = []
+    ## Start rendering
+    timeStart = time.time()
+    q_extr_list = []
+    t_extr_list = []
+    angles_list = []
+    t_list = []
+    R_extr_list = []
 
-#open(logfile, "a").close()
-#old = os.dup(1)
-#sys.stdout.flush()
-#os.close(1)
-#os.open(logfile, os.O_WRONLY)
+    # open(logfile, "a").close()
+    # old = os.dup(1)
+    # sys.stdout.flush()
+    # os.close(1)
+    # os.open(logfile, os.O_WRONLY)
 
-shape_file = "/home/ubuntu/data/Work/data/ShapeNetCore.v1/{0}/{1}/model.obj".format(CATEGORY,MODEL)
-lights_num = 20
-scene.world.light_settings.ao_factor = 0.05
+    shape_file = "/home/ubuntu/data/Work/data/ShapeNetCore.v1/{0}/{1}/model.obj".format(CATEGORY, MODEL)
+    lights_num = 20
+    scene.world.light_settings.ao_factor = 0.05
 
-# if CATEGORY == '02958343':
-#     shape_file = "/home/ubuntu/data/Work/data/ShapeNetCore.v1/{0}/{1}/model.obj".format(CATEGORY,MODEL)
-#     lights_num = 20
-#     scene.world.light_settings.ao_factor = 0.05
-# else:
-#     shape_file = "/home/ziyanw1/dataset/ShapeNetCore.v2/{0}/{1}/models/model_normalized.obj".format(CATEGORY,MODEL)
-#     lights_num = 35
-#     scene.world.light_settings.ao_factor = 0.15
-shape_file = "/home/ubuntu/data/Work/data/ShapeNetCore.v1/{0}/{1}/model.obj".format(CATEGORY,MODEL)
-lights_num = 35
-scene.world.light_settings.ao_factor = 0.15
-bpy.ops.import_scene.obj(filepath=shape_file) 
-
-print("================= OBJ file loaded sucessfully!=================")
-
-for m in bpy.data.materials:
-    m.diffuse_shader = 'LAMBERT'
-    m.diffuse_intensity = 0.5
-    m.specular_intensity = 0
-#     m.use_shadeless = True
-
-bpy.context.scene.world.light_settings.use_environment_light = True
-bpy.context.scene.world.light_settings.environment_energy = np.random.uniform(0.5, 1)
-bpy.context.scene.world.light_settings.environment_color = 'PLAIN'
-
-for i in range(15):
-    light_azimuth_deg = np.random.uniform(0, 360)
-    light_elevation_deg  = np.random.uniform(-70, 15)
-    # light_elevation_deg  = np.random.uniform(-70, 70)
-    light_dist = np.random.uniform(12, 20)
-    lx, ly, lz = obj_centered_camera_pos(light_dist, light_azimuth_deg, light_elevation_deg)
-    bpy.ops.object.lamp_add(type='POINT', view_align = False, location=(lx, ly, lz))
-    bpy.data.objects['Point'].data.energy = max(np.random.normal(20, 4),0)
-
-# uniformly sample rotation angle
-# pos = np.inf
-# while np.linalg.norm(pos)>1:
-#     pos = np.random.rand(3)*2-1
-# pos /= np.linalg.norm(pos)
-# # print(pos)
-# phi = np.arcsin(pos[2])
-# theta = np.arccos(pos[0]/np.cos(phi))
-# if pos[1]<0: theta = 2*np.pi-theta
-# elev = np.rad2deg(phi)
-# azim0 = np.rad2deg(theta)
-rho = 2
-# theta = np.random.rand()*360
-
-#azim0 = 0.
-#elev0 = 0.
-#theta0 = 0.
-# azim_all = azim0 + np.random.normal(loc=0., scale=10., size=(VIEWS,))
-azim_all = np.linspace(0, 360, 9)
-azim_all = azim_all[0:-1]
-elev_all = np.linspace(-30, 30, 5)
-
-# elev_all = elev0 + np.random.normal(loc=0., scale=10., size=(VIEWS,))
-# elev_all[0] = elev0
-
-for bkg in range(1):
-    for res in res_list:
-        scene.render.resolution_x = res
-        scene.render.resolution_y = res
-        scene.render.resolution_percentage = 100
-        base_path = BASE_OUT_DIR + 'blender_renderings/'
-        #save_path = base_path + "{2}/res{1}_continuous_0712_infiniteBkg/{0}".format(MODEL,res,CATEGORY)
-        #save_path = base_path + "{2}/res{1}_continuous_0930_persp_iphonev2/{0}".format(MODEL,res,CATEGORY)
-        save_path = base_path + "{2}/res{1}_{3}/{0}".format(MODEL,res,CATEGORY,NAME)
-        #save_path = base_path + "{2}/res{1}_debug_perspInfinite/{0}".format(MODEL,res,CATEGORY)
-        #save_path = base_path + "{2}/debug_persp/{0}".format(MODEL,res,CATEGORY)
-        if os.path.exists(save_path):
-            shutil.rmtree(save_path)
-
-        if not os.path.isdir(save_path):
-            os.makedirs(save_path)
-            
-        for i in range(azim_all.shape[0]):
-            for j in range(elev_all.shape[0]):
-                azim = azim_all[i]
-                elev = elev_all[j]
-                theta = 0.
-                camPos = objectCenteredCamPos(rho,azim,elev)
-                q1 = camPosToQuaternion(camPos)
-                q2 = camRotQuaternion(camPos,theta)
-                q = quaternionProduct(q2,q1)
-                Rt,R_extr, q_extr,t_extr, t_bcam2world = cameraExtrinsicMatrix(q,camPos)
-
-                camera.rotation_mode = "QUATERNION"
-                camera.location[0] = camPos[0] #+ t_bcam2world[0]
-                camera.location[1] = camPos[1] #+t_bcam2world[1]
-                camera.location[2] = camPos[2] #+t_bcam2world[2]
-                camera.rotation_quaternion[0] = q[0]
-                camera.rotation_quaternion[1] = q[1]
-                camera.rotation_quaternion[2] = q[2]
-                camera.rotation_quaternion[3] = q[3]
-                camera.data.sensor_height = camera.data.sensor_width
-
-                P = projectionMatrix(scene,camera)
-                # q = np.nan_to_num(q)
-                # q_extr = np.nan_to_num(q_extr)
-
-                scene.render.filepath = "{0}temp.png".format(fo.base_path)
-                bpy.ops.render.render(write_still=True)
-
-                invZ = get_inverse_depth("{0}/Z0001.exr".format(fo.base_path), res)
-                #shutil.copyfile("{0}/Z0001.exr".format(fo.base_path),"{0}/{1}_{2}.exr".format(save_path,int(azim),int(elev)))
-                #shutil.copyfile("{0}/RGB0001.exr".format(fo.base_path),"{0}/{1}_{2}_rgb.exr".format(save_path,int(azim),int(elev)))
-                sm.imsave("{0}/invZ_{1}_{2}.png".format(save_path,int(azim),int(elev)) , invZ)
-                np.save("{0}/invZ_{1}_{2}.npy".format(save_path,int(azim),int(elev)) , invZ)
-                shutil.copyfile(scene.render.filepath,"{0}/RGB_{1}_{2}.png".format(save_path,int(azim),int(elev)))
-
-                q_extr_list.append(np.array(q_extr))
-                t_extr_list.append(np.array(t_extr))
-                angles_list.append(np.stack([azim, elev, theta]))
-                R_extr_list.append(np.array(R_extr))
-
-        trans_path = "{0}/trans_per.mat".format(save_path)
-        scipy.io.savemat(trans_path,{'angles_list':np.stack(angles_list), 'R_extr_list':np.stack(R_extr_list), \
-            'q_extr_list':np.stack(q_extr_list), 't_extr_list':np.array(t_extr_list)})
-
-    #if bkg == 0:
-    #    bpy.data.objects['cobble'].select = True
-    #    bpy.ops.object.delete()
-        # bpy.data.objects['cobble'].select = False
+    # if CATEGORY == '02958343':
+    #     shape_file = "/home/ubuntu/data/Work/data/ShapeNetCore.v1/{0}/{1}/model.obj".format(CATEGORY,MODEL)
+    #     lights_num = 20
+    #     scene.world.light_settings.ao_factor = 0.05
     # else:
-    #     bpy.data.objects['cobble'].select = True
-    #     bpy.ops.object.delete()
-#os.close(1)
-#os.dup(old)
-#os.close(old)
+    #     shape_file = "/home/ziyanw1/dataset/ShapeNetCore.v2/{0}/{1}/models/model_normalized.obj".format(CATEGORY,MODEL)
+    #     lights_num = 35
+    #     scene.world.light_settings.ao_factor = 0.15
+    shape_file = "/home/ubuntu/data/Work/data/ShapeNetCore.v1/{0}/{1}/model.obj".format(CATEGORY, MODEL)
+    lights_num = 35
+    scene.world.light_settings.ao_factor = 0.15
+    bpy.ops.import_scene.obj(filepath=shape_file)
 
-# clean up
-for o in bpy.data.objects:
-    if o==camera: continue
-    o.select = True
-bpy.ops.object.delete()
-for m in bpy.data.meshes:
-    bpy.data.meshes.remove(m)
-for m in bpy.data.materials:
-    m.user_clear()
-    bpy.data.materials.remove(m)
+    print("================= OBJ file loaded sucessfully!=================")
 
-print(util.toGreen("#{3} {1} done, time={0:.4f} sec. Model saved to {2}".format(time.time()-timeStart,MODEL, trans_path, BUFFER)))
+    for m in bpy.data.materials:
+        m.diffuse_shader = 'LAMBERT'
+        m.diffuse_intensity = 0.5
+        m.specular_intensity = 0
+    #     m.use_shadeless = True
+
+    bpy.context.scene.world.light_settings.use_environment_light = True
+    bpy.context.scene.world.light_settings.environment_energy = np.random.uniform(0.5, 1)
+    bpy.context.scene.world.light_settings.environment_color = 'PLAIN'
+
+    for i in range(15):
+        light_azimuth_deg = np.random.uniform(0, 360)
+        light_elevation_deg = np.random.uniform(-70, 15)
+        # light_elevation_deg  = np.random.uniform(-70, 70)
+        light_dist = np.random.uniform(12, 20)
+        lx, ly, lz = obj_centered_camera_pos(light_dist, light_azimuth_deg, light_elevation_deg)
+        bpy.ops.object.lamp_add(type='POINT', view_align=False, location=(lx, ly, lz))
+        bpy.data.objects['Point'].data.energy = max(np.random.normal(20, 4), 0)
+
+    # uniformly sample rotation angle
+    # pos = np.inf
+    # while np.linalg.norm(pos)>1:
+    #     pos = np.random.rand(3)*2-1
+    # pos /= np.linalg.norm(pos)
+    # # print(pos)
+    # phi = np.arcsin(pos[2])
+    # theta = np.arccos(pos[0]/np.cos(phi))
+    # if pos[1]<0: theta = 2*np.pi-theta
+    # elev = np.rad2deg(phi)
+    # azim0 = np.rad2deg(theta)
+    rho = 2
+    # theta = np.random.rand()*360
+
+    # azim0 = 0.
+    # elev0 = 0.
+    # theta0 = 0.
+    # azim_all = azim0 + np.random.normal(loc=0., scale=10., size=(VIEWS,))
+    azim_all = np.linspace(0, 360, 9)
+    azim_all = azim_all[0:-1]
+    elev_all = np.linspace(-30, 30, 5)
+
+    # elev_all = elev0 + np.random.normal(loc=0., scale=10., size=(VIEWS,))
+    # elev_all[0] = elev0
+
+    for bkg in range(1):
+        for res in res_list:
+            scene.render.resolution_x = res
+            scene.render.resolution_y = res
+            scene.render.resolution_percentage = 100
+            base_path = BASE_OUT_DIR + 'blender_renderings/'
+            # save_path = base_path + "{2}/res{1}_continuous_0712_infiniteBkg/{0}".format(MODEL,res,CATEGORY)
+            # save_path = base_path + "{2}/res{1}_continuous_0930_persp_iphonev2/{0}".format(MODEL,res,CATEGORY)
+            save_path = base_path + "{2}/res{1}_{3}/{0}".format(MODEL, res, CATEGORY, NAME)
+            # save_path = base_path + "{2}/res{1}_debug_perspInfinite/{0}".format(MODEL,res,CATEGORY)
+            # save_path = base_path + "{2}/debug_persp/{0}".format(MODEL,res,CATEGORY)
+            if os.path.exists(save_path):
+                shutil.rmtree(save_path)
+
+            if not os.path.isdir(save_path):
+                os.makedirs(save_path)
+
+            for i in range(azim_all.shape[0]):
+                for j in range(elev_all.shape[0]):
+                    azim = azim_all[i]
+                    elev = elev_all[j]
+                    theta = 0.
+                    camPos = objectCenteredCamPos(rho, azim, elev)
+                    q1 = camPosToQuaternion(camPos)
+                    q2 = camRotQuaternion(camPos, theta)
+                    q = quaternionProduct(q2, q1)
+                    Rt, R_extr, q_extr, t_extr, t_bcam2world = cameraExtrinsicMatrix(q, camPos)
+
+                    camera.rotation_mode = "QUATERNION"
+                    camera.location[0] = camPos[0]  # + t_bcam2world[0]
+                    camera.location[1] = camPos[1]  # +t_bcam2world[1]
+                    camera.location[2] = camPos[2]  # +t_bcam2world[2]
+                    camera.rotation_quaternion[0] = q[0]
+                    camera.rotation_quaternion[1] = q[1]
+                    camera.rotation_quaternion[2] = q[2]
+                    camera.rotation_quaternion[3] = q[3]
+                    camera.data.sensor_height = camera.data.sensor_width
+
+                    P = projectionMatrix(scene, camera)
+                    # q = np.nan_to_num(q)
+                    # q_extr = np.nan_to_num(q_extr)
+
+                    scene.render.filepath = "{0}temp.png".format(fo.base_path)
+                    bpy.ops.render.render(write_still=True)
+
+                    invZ = get_inverse_depth("{0}/Z0001.exr".format(fo.base_path), res)
+                    # shutil.copyfile("{0}/Z0001.exr".format(fo.base_path),"{0}/{1}_{2}.exr".format(save_path,int(azim),int(elev)))
+                    # shutil.copyfile("{0}/RGB0001.exr".format(fo.base_path),"{0}/{1}_{2}_rgb.exr".format(save_path,int(azim),int(elev)))
+                    sm.imsave("{0}/invZ_{1}_{2}.png".format(save_path, int(azim), int(elev)), invZ)
+                    np.save("{0}/invZ_{1}_{2}.npy".format(save_path, int(azim), int(elev)), invZ)
+                    shutil.copyfile(scene.render.filepath,
+                                    "{0}/RGB_{1}_{2}.png".format(save_path, int(azim), int(elev)))
+
+                    q_extr_list.append(np.array(q_extr))
+                    t_extr_list.append(np.array(t_extr))
+                    angles_list.append(np.stack([azim, elev, theta]))
+                    R_extr_list.append(np.array(R_extr))
+
+            trans_path = "{0}/trans_per.mat".format(save_path)
+            scipy.io.savemat(trans_path, {'angles_list': np.stack(angles_list), 'R_extr_list': np.stack(R_extr_list), \
+                                          'q_extr_list': np.stack(q_extr_list), 't_extr_list': np.array(t_extr_list)})
+
+        # if bkg == 0:
+        #    bpy.data.objects['cobble'].select = True
+        #    bpy.ops.object.delete()
+        # bpy.data.objects['cobble'].select = False
+        # else:
+        #     bpy.data.objects['cobble'].select = True
+        #     bpy.ops.object.delete()
+    # os.close(1)
+    # os.dup(old)
+    # os.close(old)
+
+    # clean up
+    for o in bpy.data.objects:
+        if o == camera: continue
+        o.select = True
+    bpy.ops.object.delete()
+    for m in bpy.data.meshes:
+        bpy.data.meshes.remove(m)
+    for m in bpy.data.materials:
+        m.user_clear()
+        bpy.data.materials.remove(m)
+
+    print(util.toGreen(
+        "#{3} {1} done, time={0:.4f} sec. Model saved to {2}".format(time.time() - timeStart, MODEL, trans_path,
+                                                                     BUFFER)))
+
+
+if __name__=="__main__":
+    d = {
+        "02691156": "airplane",
+        "03001627": "chair",
+        "03636649": "lamp",
+        "04379243": "table",
+        "04530566": "vessel",
+    }
+    for cat_num in d.keys():
+        run(cat_num, "lists/%s_lists", d[cat_num])
